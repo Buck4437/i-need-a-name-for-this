79102b8b5b4041625cace35ed90036a0d82ea6a7
Buck4437 <Buck4437@users.noreply.github.com>
2020-03-17 02:20:18 +0800

    The first dim layer is done. Creation of further layer would be much easier

--- js/maingame.js
+++ js/maingame.js
@@ -1,58 +1,69 @@
 var player = {
-  money:1,
-  basecost:[null,1,10,100],
-  basecostincrease:[null,2,10,50],
-  multiplier:[null,1,1,1],
-  multiplierincrease:[null,1.5,1.5,1.5],
-  layers:{
-    dim1:{
-      index:1,
-      amount:0,
-      bought:0,
-      cost:1
-    },
-    dim2:{
-      index:2,
-      amount:0,
-      bought:0,
-      cost:10
-    },
-    dim3:{
-      index:3,
-      amount:0,
-      bought:0,
-      cost:100
+    money:1,
+    layers:{
+      dimlayer1:{
+        amount:0,
+        bought:0,
+        cost:1,
+        multi:1,
+        basecost:1,
+        costincrease:5,
+        multiincrease:1.5
+      }
     }
-  }
+  };
+var updaterate = 50;
+
+//produces money according to a set interval
+function producemoney(){
+  player.money += player.layers["dimlayer1"].amount * player.layers["dimlayer1"].multi * updaterate / 1000;
 };
 
 //update money
 function updatemoney(){
-  document.getElementById('player.money').innerHTML = player.money;
+  document.getElementById('player.money').innerHTML = Math.round(player.money * 10) / 10;
 };
 
 //update cost
 function updatecost(layername){
-  document.getElementById('cost'+layername).innerHTML = player.layers[layername].cost;
+  document.getElementById(layername + 'cost').innerHTML = Math.round(player.layers[layername].cost);
 };
 
 //update multi
-function updatemulti(layername, index){
-  document.getElementById('multiplier'+layername).innerHTML = player.multiplier[index];
+function updatemulti(layername){
+  document.getElementById(layername + 'multi').innerHTML = "x" + Math.round(player.layers[layername].multi * 10) / 10;
+};
+
+//update bought
+function updatebought(layername){
+  document.getElementById(layername + 'bought').innerHTML = "(" + Math.round(player.layers[layername].bought) + ")";
+};
+
+//update amount
+function updateamount(layername){
+  document.getElementById(layername + 'amount').innerHTML = Math.round(player.layers[layername].amount);
 };
 
 //buy layers
 function buylayer(layername){
-  index = player.layers[layername].index
-  if (player.money >= player.layers[layername].cost){
-    player.money = player.money - player.layers[layername].cost
-    player.layers[layername].cost = player.layers[layername].cost * player.basecostincrease[index]
-    player.multiplier[index] = player.multiplier[index] * player.multiplierincrease[index]
-    updatemoney();
+  if(player.money >= player.layers[layername].cost){
+    player.money -= player.layers[layername].cost;
+    player.layers[layername].amount ++;
+    player.layers[layername].bought ++;
+    player.layers[layername].cost *= player.layers[layername].costincrease;
+    player.layers[layername].multi *= player.layers[layername].multiincrease;
+    //updatemoney handled by update()
     updatecost(layername);
-    updatemulti(layername, index);
+    updatemulti(layername);
+    updatebought(layername);
+    updateamount(layername);
   };
 };
 
+//update everything that ran on set timed interval
 
-updatemoney();
+
+setInterval(function update(){
+             producemoney();
+             updatemoney();
+           },50);
